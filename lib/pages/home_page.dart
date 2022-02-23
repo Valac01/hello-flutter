8fd3a31b49dc6651caedb2854b0a7b15ca7b9c51
Barish Barla <barish.barla10@gmail.com>
2022-02-23 20:46:33 +0530

    Added Items List

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/models/catalog.dart';
 import 'package:hello_flutter/widgets/drawer.dart';
+import 'package:hello_flutter/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Catalog App",
         ),
       ),
-      body: Center(
-        child: Image.asset("assets/images/home.png"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       drawer: Mydrawer(),
     );
